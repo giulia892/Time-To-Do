67961d90910702768f1a27ca8805bf230cd8d130
giulia892 <giloffredo02@gmail.com>
2024-12-18 11:43:05 +0100

    first commit

---
+++ TimeToDo/TimeToDo/TimeToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToDoApp.swift
+//  TimeToDo
+//
+//  Created by Giulia Loffredo on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
